959cc2a10b53a19bf3324d9d675259a9d48ae744
Laure Leter <laure.leter@gmail.com>
2016-02-16 15:03:44 +0100

    liens about & story

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
+  get 'about' => "pages#about"
+  get 'story' => "pages#story"
   resources :offers do
     resources :orders
   end
