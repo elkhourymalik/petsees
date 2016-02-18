407fc3eb162db8f11a5cc3e18437fcb46646f830
Clémentine Guyon <clementine.guyon@outlook.com>
2016-02-18 15:40:37 +0100

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,3 @@
 //= require gmaps/google
 //= require bootstrap-datepicker
 
-$('#sandbox-container .input-daterange').datepicker({
-    weekStart: 1,
-    language: "fr",
-    todayHighlight: true
-});
